21bc1824da744542003d746f8ab6ff2286b95e32
dogood1419 <dogood1419@gmail.com>
2023-12-12 10:16:09 +0900

    화요일 1교시

--- function3.py
+++ function3.py
@@ -19,18 +19,31 @@
 
 # 반복구문
 
-value = 5
-while value > 0 :
-    print (value)
-    value -= 1
+# value = 5
+# while value > 0 :
+#     print (value)
+#     value -= 1
 
-fruits = {'apple':10, 'kiwi':20}
-for item in fruits.items():
-    print(item)
+# fruits = {'apple':10, 'kiwi':20}
+# for item in fruits.items():
+#     print(item)
 
-print("---list 내장----")
-lst = list (range(1,11))
-print ([i**2 for i in lst if i >5])
+# print("---list 내장----")
+# lst = list (range(1,11))
+# print ([i**2 for i in lst if i >5])
 
-tp = ('apple', 'kiwi', 'pineapple')
-print ([len(i) for i in tp])
+# tp = ('apple', 'kiwi', 'pineapple')
+# print ([len(i) for i in tp])
+
+# 구구단 출력
+# for i in range(2,10):
+#     print ('{0}단 출력'.format(i))
+#     for j in range(1,10):
+#         print("{0}*{1} = {2}".format(i,j,i*j))
+
+# f-string 문법을 사용
+for i in range(2,10):
+    print (f'{i}단 출력')
+    for j in range(1,10):
+        print(f"{i}*{j} = {i*j}")
+
